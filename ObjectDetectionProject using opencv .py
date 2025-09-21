51326c31403e29eb20527714e796285cc2e18e7c
PrincePoudel-Anjal <prince12poudel@gmail.com>
2025-09-21 12:03:22 +0545

    Update ObjectDetectionProject using opencv .py

--- ObjectDetectionProject using opencv .py
+++ ObjectDetectionProject using opencv .py
@@ -8,6 +8,7 @@ webcam = cv2.VideoCapture(0)
 
 while True:
     ret,image = webcam.read()
+    image = cv2.resize(image,(600,800))
     hsvcimage = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
     lowerlimit = np.array([22, 100, 100])
     upperlimit = np.array([30, 255, 255])
@@ -16,28 +17,16 @@ while True:
 
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        x1,y1,h,w = cv2.boundingRect(cnt)
-        cv2.rectangle(image,(x1,y1),(x1+h,y1+w),(0,255,0),3)
+        area = cv2.contourArea(cnt)
+
+        if area>2000:
+            x1, y1, h, w = cv2.boundingRect(cnt)
+            cv2.rectangle(image, (x1, y1), (x1 + h, y1 + w), (0, 255, 0), 3)
+
+
 
     cv2.imshow('Window1', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
